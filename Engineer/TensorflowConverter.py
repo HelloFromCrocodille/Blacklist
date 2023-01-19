c1429245584b80b8ba5773af4a96c78e645deae3
Mobazene <122552875+Mobazene@users.noreply.github.com>
2023-01-19 16:35:13 +0700

    add : Tensorflow Converter

---
+++ Engineer/TensorflowConverter.py
@@ -0,0 +1,11 @@
+import tensorflow as tf
+
+class Converter:
+    def __init__(self, model):
+        self.model = model
+
+    def TFLite(self):
+        pass
+
+    def TFJavaScript(self):
+        pass
