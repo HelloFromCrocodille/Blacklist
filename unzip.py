27da47ed4d430c0bda1c9f4d8d4f692607f2ae55
Mobazene <122552875+Mobazene@users.noreply.github.com>
2023-01-19 23:57:23 +0700

    rename

--- unzip.py
+++ unzip.py
@@ -1,6 +1,6 @@
 import zipfile
 
-def Unzip(filename, new_name=None):
+def unzip(filename, new_name=None):
     file_zip = zipfile.ZipFile(filename, 'r')
     file_zip.extractall()
     file_zip.close()
